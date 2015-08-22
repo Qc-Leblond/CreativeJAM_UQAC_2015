e5a5299ebc603d88fd6a7cebf8dc41986e71979b
Antoine Brouillette <antoineqc@hotmail.com>
2015-08-22 00:20:10 -0400

    State Machine Completed, working on tests to switch states

---
+++ CreativeJAM_2015/Assets/Scripts/AI Related/AI_DestinationPointsList.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AI_DestinationPointsList {
+
+    List<AI_DestinationPoint> freeDestinationPoint;
+    List<AI_DestinationPoint> occupiedDestinationPoint = new List<AI_DestinationPoint>();
+
+    public AI_DestinationPointsList(AI_DestinationPoint[] array) {
+        freeDestinationPoint = new List<AI_DestinationPoint>(array);
+    }
+
+    public AI_DestinationPoint GetRandomPoint(AI_DestinationPoint currentPoint) {
+        List<AI_DestinationPoint> destinations = new List<AI_DestinationPoint>();
+        for (int i = 0; i < freeDestinationPoint.Count; i++) {
+            if (freeDestinationPoint[i].isActive)
+                destinations.Add(freeDestinationPoint[i]);
+        }
+        AI_DestinationPoint destination = destinations[Random.Range(0, destinations.Count)];
+        //Change Lists
+        if (occupiedDestinationPoint.Contains(currentPoint)) {
+            occupiedDestinationPoint.Remove(currentPoint);
+            freeDestinationPoint.Add(currentPoint);
+        }
+        occupiedDestinationPoint.Add(destination);
+        freeDestinationPoint.Remove(destination);
+        return destination;
+    }
+}
---
+++ CreativeJAM_2015/Assets/Scripts/AI Related/FieldOfView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldOfView : MonoBehaviour {
+
+    Girl_AI girlAI;
+
+    void Awake() {
+        girlAI = GetComponentInParent<Girl_AI>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            girlAI.SwitchState(Girl_AI.State.doubtful);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.tag == "Player") {
+            girlAI.GoBackToPreviousState();
+        }
+    }
+}
