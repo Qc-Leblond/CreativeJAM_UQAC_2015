27c1a5776584a42b8299ae673d801ae0cedb2370
Antoine Brouillette <antoineqc@hotmail.com>
2015-08-22 03:43:32 -0400

    Mouvement and pathfinding completed + Working on the AI field of view

--- CreativeJAM_2015/Assets/Scripts/AI Related/FieldOfView.cs
+++ CreativeJAM_2015/Assets/Scripts/AI Related/FieldOfView.cs
@@ -4,20 +4,40 @@ using System.Collections;
 public class FieldOfView : MonoBehaviour {
 
     Girl_AI girlAI;
+    Transform player;
+    Transform girl;
+    bool doubtful;
 
     void Awake() {
         girlAI = GetComponentInParent<Girl_AI>();
+        girl = girlAI.transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        doubtful = false;
     }
 
-    void OnTriggerEnter(Collider other) {
+    void OnTriggerStay(Collider other) {
+        Debug.Log("Test");
         if (other.tag == "Player") {
-            girlAI.SwitchState(Girl_AI.State.doubtful);
+            Debug.Log("Test");
+            RaycastHit hit;
+            Physics.Raycast(girl.position, player.position - girl.position, out hit);
+            if (hit.rigidbody.transform.root.tag == "Player") {
+                girlAI.SwitchState(Girl_AI.State.doubtful);
+                doubtful = true;
+            }
+            else {
+                girlAI.GoBackToPreviousState();
+                doubtful = false;
+            }
         }
     }
 
     void OnTriggerExit(Collider other) {
-        if (other.tag == "Player") {
+        Debug.Log("Test");
+        if (other.tag == "Player" && doubtful) {
             girlAI.GoBackToPreviousState();
+            doubtful = false;
         }
     }
+
 }
