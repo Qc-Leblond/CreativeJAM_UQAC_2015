afb7e95b2c67379b81385b7dd6b8e4e6e0627a76
Nic007 <nic007nc@gmail.com>
2015-08-22 00:47:36 -0400

    allo je suis james bound et jai fais le player script et modifier pour la 1er fois le gameManager

--- CreativeJAM_2015/Assets/Scripts/GameManager.cs
+++ CreativeJAM_2015/Assets/Scripts/GameManager.cs
@@ -2,6 +2,77 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+    //Dans le GameManager on gere le singleton......
+
+    //Declaration de variable
+    //********************************************************************************************//
+        public static GameManager instance;
+        
+        //Variable public qui contient le joueur (On le trouve directement dans l'interface de unity)
+        public GameObject playerGO;
+
+        //Varaible qui contient le scrpit du player
+        private Player playerScript;
+
+        //Variable qui contient le doute de sur le joueur (doubt)
+        private float doubt;    
+
+        //Variable pour le get et set le doubt
+        public float getSetDoubt
+        {
+            get
+            {
+                return doubt;
+            }
+            private set
+            {
+                //Ici si jamais on veut gere quoi faire avant que le doubt se rendre soit <= 0
+                Debug.Log("Valeur du doute :" + value);
+                doubt = value;
+            }
+        }
+        
+    //********************************************************************************************//
+
+    //Dans le Awake on gere la creation du singleton GameManager
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = new GameManager();
+        }
+
+        playerScript = playerGO.GetComponent<Player>();
+
+        initiatePlayersStates(playerScript);
+
+        doubt = 0;
+        
+    }
+
+    void update()
+    {
+        if (playerScript.returnIsGirlWalking())
+        {
+            //On doit descendre l'ego du joueur
+            playerScript.ego = playerScript.ego - 0.5f;//Valeur temporaire
+
+            //On verifie si l'ego est = 0
+            if (playerScript.ego <= 0)
+            {
+                Debug.Log("TU ES UNE FILLE !!!");
+            }
+        }
+    }
+
+    //Fonction pour initialiser les info sur le joueur ego, isGirlWalking etc...
+    void initiatePlayersStates(Player thePlayerScript)
+    {
+        thePlayerScript.nbrJunk = 0; //Valeur temporaire
+        thePlayerScript.isGirlWalking = false;
+        thePlayerScript.ego = 100f; //Valeur temporaire
+    }
+
 
 	
 }
---
+++ CreativeJAM_2015/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    //Declaration de variable
+    //********************************************************************************************//
+        
+        //Variable qui represente les points "ego" du personnage. Une gauge representera sa valeur pour le joueur
+        public float ego;
+
+        //Variable qui sert a savoir si le joueur est entrain de marcher comme un fille (Cette variable sera modifier directement par le controler avec une fonction du genre SET)
+        public bool isGirlWalking;
+
+        //Variable qui garde en memoire le nombre de Junk que le joueur a amasser.
+        public int nbrJunk;
+    //********************************************************************************************//
+  
+
+	// Use this for initialization
+	void Awake () {
+
+    }
+
+    void update()
+    {
+
+    }
+
+    //Fonction qui retourne si le personnage est entrain de marcher comme une fille
+    public bool returnIsGirlWalking()
+    {
+        return isGirlWalking;
+    }
+
+    //Fonction qui retourne le nbr de Junk que le personnage a amasser (nbrJunk)
+    public int returnNbrJunk()
+    {
+        return nbrJunk;
+    }
+
+    //Fonction qui "set" le nbr de Junk (nbrJunk) quand le joueur en amasse un tas (recois en paramtre le nbr de junk a ajouter)
+    public void setNbrJunk(int gainJunk)
+    {
+        nbrJunk = nbrJunk + gainJunk;
+    }    
+}
