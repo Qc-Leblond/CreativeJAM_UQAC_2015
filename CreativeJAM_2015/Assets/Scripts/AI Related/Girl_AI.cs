5d787db30a1491ba735bab11eae5f3fd8737175c
Antoine Brouillette <antoineqc@hotmail.com>
2015-08-22 03:43:32 -0400

    Mouvement and pathfinding completed + Working on the AI field of view

--- CreativeJAM_2015/Assets/Scripts/AI Related/Girl_AI.cs
+++ CreativeJAM_2015/Assets/Scripts/AI Related/Girl_AI.cs
@@ -24,6 +24,10 @@ public class Girl_AI : MonoBehaviour {
     public static AI_DestinationPointsList possibleDestinations;
     [HideInInspector]
     public AI_DestinationPoint currentPos;
+    [Header("Field of view")]
+    public MeshRenderer fieldOfViewVisual;
+    public Color noDoubt;
+    public Color isDoubting;
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -37,26 +41,31 @@ public class Girl_AI : MonoBehaviour {
         stateDictionary.Add(State.occupied, new AIState_Occupied(this));
         stateDictionary.Add(State.doubtful, new AIState_Doubtful(this));
         SwitchState(State.idle);
+        fieldOfViewVisual.material.color = noDoubt;
     }
 
     void Update() {
         if (!isCrying) {
             currentState.Running();
         }
+
+        if (Input.GetKeyDown(KeyCode.T)) SwitchState(State.doubtful);
     }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     public void SwitchState(State state) {
-        if (currentState != null) {
-            currentState.Finish();
-            previousState = currentState;
+        if (!isCrying) {
+            if (currentState != null) {
+                currentState.Finish();
+                previousState = currentState;
+            }
+            else {
+                previousState = stateDictionary[State.idle];
+            }
+            currentState = stateDictionary[state];
+            currentState.Execute();
         }
-        else {
-            previousState = stateDictionary[State.idle];
-        }
-        currentState = stateDictionary[state];
-        currentState.Execute();
     }
 
     public void GoBackToPreviousState() {
@@ -118,7 +127,7 @@ public class AIState_Idle : AIState {
     float timeBeforeMove;
     public AIState_Idle(Girl_AI ai) : base(ai) { }
     public override void Execute() {
-        timeBeforeMove = Random.Range(5f, 60f); //In secondes
+        timeBeforeMove = Random.Range(5f, 30f); //In secondes
     }
     public override void Running() {
         timeBeforeMove -= Time.deltaTime;
@@ -151,8 +160,16 @@ public class AIState_Occupied : AIState {
 /// </summary>
 public class AIState_Doubtful : AIState {
     public AIState_Doubtful(Girl_AI ai) : base(ai) {  }
+    public override void Execute() {
+        girlAI.fieldOfViewVisual.material.color = girlAI.isDoubting;
+        girlAI.girlPathingAI.Stop();
+    }
     public override void Running() {
-        //TODO Add to doubt meter in GameManager
+        //Add to doubt
+    }
+    public override void Finish() {
+        girlAI.fieldOfViewVisual.material.color = girlAI.noDoubt;
+        girlAI.girlPathingAI.Resume();
     }
 }
 
