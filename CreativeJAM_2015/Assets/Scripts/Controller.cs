0a3d798310751ec3880f2999b3f36fe3e49f98f1
Mathieu <mathieu.naud@hotmail.com>
2015-08-22 03:52:57 -0400

    Controller qui avance encore plus!

--- CreativeJAM_2015/Assets/Scripts/Controller.cs
+++ CreativeJAM_2015/Assets/Scripts/Controller.cs
@@ -15,30 +15,35 @@ public class Controller : MonoBehaviour
 
     void Awake()
     {
+        Cursor.visible = false; 
         player = this.GetComponent<Player>();
         isAxisInUse = false;
     }
 
+    void FixedUpdate()
+    {
+        float vCam = Input.GetAxis("VerticalCamera");
+        if (Camera.transform.rotation.x < MaxCam && vCam > 0 || Camera.transform.rotation.x > MinCam && vCam < 0)
+            Camera.transform.Rotate(Vector3.right * TurnSpeed * vCam * Time.deltaTime);   ///caméra haut-bas
+    }
 	void Update() 
     {
         //Déplacements du personnage
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         float hCam = Input.GetAxis("HorizontalCamera");
-        float vCam = Input.GetAxis("VerticalCamera");
+        //float vCam = Input.GetAxis("VerticalCamera");
 
         transform.Translate(-Vector3.forward * MoveSpeed * v * Time.deltaTime); //avant-arrière
         transform.Translate(Vector3.right * MoveSpeed * h * Time.deltaTime);    //gauche-droite
         transform.Rotate(Vector3.up * TurnSpeed * hCam * Time.deltaTime);       //caméra gauche-droite
-        if (Camera.transform.rotation.x < MaxCam && vCam > 0 || Camera.transform.rotation.x > MinCam && vCam < 0)
-            Camera.transform.Rotate(Vector3.right * TurnSpeed * vCam * Time.deltaTime);   ///caméra haut-bas
+        
 
         //marche coquine lorsque B est appuyé
         if (Input.GetButtonDown("GirlWalk"))
         {
             player.isGirlWalking = true;
-            MoveSpeed = 3f;
-            Debug.Log("3");
+            MoveSpeed = 2f;
         }
 
         //fin de la marche coquine lorsque B est relâché
@@ -46,25 +51,43 @@ public class Controller : MonoBehaviour
         {
             player.isGirlWalking = false;
             MoveSpeed = 5f;
-            Debug.Log("5");
         }
 
-        //Sélection des objets
-        if (Input.GetAxisRaw("SelectUp") > 0 && objectSelected < NbrObjet && !isAxisInUse)
+        //Sélection des objets par les triggers
+        if (Input.GetAxisRaw("SelectTrigger") == -1 && objectSelected < NbrObjet && !isAxisInUse)
         {
             isAxisInUse = true;
             objectSelected++;
-            Debug.Log(objectSelected);
         }
 
-        if (Input.GetAxisRaw("SelectDown") < 0 && objectSelected > 0 && !isAxisInUse)
+        if (Input.GetAxisRaw("SelectTrigger") == 1 && objectSelected > 0 && !isAxisInUse)
         {
             isAxisInUse = true;
             objectSelected--;
-            Debug.Log(objectSelected);
         }
 
-        if (Input.GetAxisRaw("SelectDown") == 0)
+        if (Input.GetAxis("SelectTrigger") < 1 && Input.GetAxis("SelectTrigger") > -1)
             isAxisInUse = false;
+
+        //Sélection des objects par Q et E
+        if (Input.GetKeyDown(KeyCode.Q) && objectSelected > 0)
+            objectSelected--;
+
+        if (Input.GetKeyDown(KeyCode.E) && objectSelected < NbrObjet)
+            objectSelected++;
+
+        //Input pour crafter un item
+        if (Input.GetButtonDown("CraftItem"))
+        {
+            //Do something fun :D
+            Debug.Log("craft");
+        }
+
+        //Input pour utiliser un item
+        if (Input.GetButtonDown("UseItem"))
+        {
+            Debug.Log("use");
+            //Do something fun :D
+        }
 	}
 }
