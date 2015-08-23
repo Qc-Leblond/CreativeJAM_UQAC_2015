737378a9559fdb4c3b88723c70383ecd444b1380
Karibou <odricplamondon@hotmail.com>
2015-08-22 23:05:54 -0400

    Travailler sur interface (bcp dans inventaire)

--- CreativeJAM_2015/Assets/Scripts/Inventory.cs
+++ CreativeJAM_2015/Assets/Scripts/Inventory.cs
@@ -17,6 +17,15 @@ public class Inventory : MonoBehaviour {
 
     public GameObject[] inventory = new GameObject[4];
     public GameObject[] btnA = new GameObject[4];
+    public GameObject[] Use = new GameObject[4];
+    public GameObject[] panel = new GameObject[4];
+
+    public GameObject[] craftTable = new GameObject[4];
+    public GameObject[] btnX = new GameObject[4];
+    public GameObject[] Craft = new GameObject[4];
+    public GameObject[] panelCraft = new GameObject[4];
+
+    public GameObject CraftingActive;
 
 	// Use this for initialization
 	void Start () {
@@ -29,7 +38,7 @@ public class Inventory : MonoBehaviour {
 
         indexObjectSelected = 0;
         //inventory[indexObjectSelected].GetComponent<Image>().color = new Color(255, 255, 255);
-        changeColorAlphaAndBtnA();
+        changeColorAlphaAndBtn(panel, btnA, Use, inventory);
 
         Cursor.visible = false;
 
@@ -60,28 +69,44 @@ public class Inventory : MonoBehaviour {
             }
         }
 
-        changeColorAlphaAndBtnA();
+        changeColorAlphaAndBtn(panel, btnA, Use, inventory);
+        
+        if(CraftingActive.gameObject.activeSelf)
+        {
+            changeColorAlphaAndBtn(panelCraft, btnX, Craft, craftTable);
+            Debug.Log("allo");
+
+        }
         
     }
 
-    void changeColorAlphaAndBtnA()
+    void changeColorAlphaAndBtn(GameObject[] lesPanels,GameObject[] lesBtn,GameObject[] lesString, GameObject[] lesInventaire)
     {
         for (int x = 0; x < 4; x++)
         {
             if (x == indexObjectSelected)
             {
                 //Debug.Log(x);
-                inventory[indexObjectSelected].GetComponent<Image>().color = new Color(255, 255, 255);
+                //inventory[indexObjectSelected].GetComponent<Image>().color = new Color(255, 255, 255);
+                lesPanels[indexObjectSelected].GetComponentInChildren<Image>().color = new Color(255, 255, 255);
                 //Debug.Log(inventory[indexObjectSelected].GetComponent<Image>().color);
-                btnA[indexObjectSelected].GetComponent<Image>().color = new Color(255, 255, 255);
-                
+                lesBtn[indexObjectSelected].GetComponent<Image>().color = new Color(255, 255, 255);
+                //btnA[indexObjectSelected].GetComponentInChildren<Image>().color = new Color(255, 255, 255);
+                lesString[indexObjectSelected].GetComponent<Text>().color = new Color(0, 0, 0);
+                lesInventaire[indexObjectSelected].GetComponentInChildren<Image>().color = new Color(255, 255, 255);
+
             }
             else
             {
                 //Debug.Log("Else");
-                inventory[x].GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f, 1);
+                //inventory[x].GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f, 1);
                 //Debug.Log(inventory[x].GetComponent<Image>().color);
-                btnA[x].GetComponent<Image>().color = new Color(0, 0, 0, 0);
+                lesBtn[x].GetComponent<Image>().color = new Color(0, 0, 0, 0);
+                lesInventaire[x].GetComponentInChildren<Image>().color = new Color(0.5f, 0.5f, 0.5f, 1);
+                //btnA[x].GetComponentInChildren<Image>().color = new Color(0, 0, 0, 0);
+                lesString[x].GetComponent<Text>().color = new Color(0, 0, 0, 0);
+                lesPanels[x].GetComponentInChildren<Image>().color = new Color(0, 0, 0, 0);
+
             }
         }
     }
