1af7ceacc099902b52223d81a2812cf530e0a694
Karibou <odricplamondon@hotmail.com>
2015-08-23 00:42:49 -0400

    Pour donner le prefab

---
+++ CreativeJAM_2015/Assets/Scripts/CraftTable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraftTable : MonoBehaviour {
+
+    public GameObject craftingUI;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "craftingTable")
+        {
+            craftingUI.SetActive(true);
+        }
+    }
+}
